7821e6d5dd384e0753eb680d1f22398fd3855a53
Df7Z <91566826+Df7Z@users.noreply.github.com>
2024-08-10 17:13:24 +0300

    fix 2

---
+++ Assets/Scripts/PCDebug.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PCDebug : MonoBehaviour
+{
+    public UnityEngine.UI.Button Left;
+    public UnityEngine.UI.Button Right;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Left.onClick.Invoke();
+        }
+        
+        if (Input.GetKey(KeyCode.D))
+        {
+            Right.onClick.Invoke();
+        }
+    }
+}
